87473fe33e3aa7824bf823144b4117f2edba29f0
Alex Kytsun <kytsun.sanya@gmail.com>
2022-05-24 20:32:53 +0300

    added Product classes

---
+++ Product/Product.cs
@@ -0,0 +1,87 @@
+﻿namespace ProductClasses
+{
+    class Product
+    {
+        public string name;
+        public double price;
+        public double weight;
+
+        public Product(string name, double price, double weight)
+        {
+            this.name = name;
+            this.price = price;
+            this.weight = weight;
+        }
+
+        public void ChangePrice(double percentage)
+        {
+            this.price = this.price * (percentage / 100);
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.name}\nPrice: {this.price}\nWeight: {this.weight}";
+        }
+    }
+
+    class Meat : Product
+    {
+        public string category;
+        public string type;
+        public Meat(string name, double price, double weight, string category, string type) : base(name, price, weight)
+        {
+            this.name = name;
+            this.price = price;
+            this.weight = weight;
+            this.category = category;
+            this.type = type;
+        }
+    }
+
+    class DairyProduct : Product
+    {
+        public int expirationDate;
+        public DairyProduct(string name, double price, double weight, int expirationDate) : base(name, price, weight)
+        {
+            this.name = name;
+            this.price = price;
+            this.weight = weight;
+            this.expirationDate = expirationDate;
+        }
+
+        public void ChangePrice(double percentage)
+        {
+            this.price = this.price * ((percentage - expirationDate) / 100);
+        }
+    }
+
+    class Buy
+    {
+        public int quantity;
+        public double order_total;
+        public double order_weight;
+
+        public Buy(int quantity, double order_total, double order_weight)
+        {
+            this.quantity = quantity;
+            this.order_total = order_total;
+            this.order_weight = order_weight;
+        }
+    }
+
+    class Check
+    {
+        public void showProduct(Product p)
+        {
+            Console.WriteLine($"Name: {p.name}");
+            Console.WriteLine($"Price: {p.price}");
+            Console.WriteLine($"Weight: {p.weight}");
+        }
+        public void showOrder(Buy b)
+        {
+            Console.WriteLine($"Quantity: {b.quantity}");
+            Console.WriteLine($"Total Price: {b.order_total}");
+            Console.WriteLine($"Order Weight: {b.order_weight}");
+        }
+    }
+}
---
+++ Product/Storage.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+
+namespace ProductClasses
+{
+    class Storage
+    {
+        List<Product> Products = new List<Product>();
+
+        public static Product newProduct()
+        {
+            Console.Write("Name of Product: ");
+            string name = Console.ReadLine();
+            Console.Write("Price: ");
+            double price = Double.Parse(Console.ReadLine());
+            Console.Write("Weight: ");
+            double weight = Double.Parse(Console.ReadLine());
+
+            return new Product(name, price, weight);
+        }
+        public static Buy newOrder()
+        {
+            Console.Write("Quantity: ");
+            int quantity = Int32.Parse(Console.ReadLine());
+            Console.Write("Price: ");
+            double price = Double.Parse(Console.ReadLine());
+            Console.Write("Weight: ");
+            double weight = Double.Parse(Console.ReadLine());
+
+            return new Buy(quantity, price, weight);
+        }
+        public static Meat newMeat()
+        {
+            Console.Write("Name of Product: ");
+            string name = Console.ReadLine();
+            Console.Write("Price: ");
+            double price = Double.Parse(Console.ReadLine());
+            Console.Write("Weight: ");
+            double weight = Double.Parse(Console.ReadLine());
+            Console.Write("Category: ");
+            string category = Console.ReadLine();
+            Console.Write("Type: ");
+            string type = Console.ReadLine();
+
+            return new Meat(name, price, weight, category, type);
+        }
+        public static DairyProduct newDairyProduct()
+        {
+            Console.Write("Name of Product: ");
+            string name = Console.ReadLine();
+            Console.Write("Price: ");
+            double price = Double.Parse(Console.ReadLine());
+            Console.Write("Weight: ");
+            double weight = Double.Parse(Console.ReadLine());
+            Console.Write("Expiration Date: ");
+            int expirationDate = Int32.Parse(Console.ReadLine());
+
+            return new DairyProduct(name, price, weight, expirationDate);
+        }
+
+        public void printAllProducts()
+        {
+            foreach (Product prod in Products)
+            {
+                prod.ToString();
+            }
+        }
+        public void changePrices(double percentage)
+        {
+            foreach (Product prod in Products)
+            {
+                prod.ChangePrice(percentage);
+            }
+        }
+    }
+}
